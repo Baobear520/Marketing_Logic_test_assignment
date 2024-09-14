28e1fd2d14de5ad142a83241e34440383d8769ce
Dima <aldmikon@gmail.com>
2024-09-14 16:51:30 +0800

    Refactoring Python task 2 main script (adding class Selection)

--- Python_tasks/task_2/main.py
+++ Python_tasks/task_2/main.py
@@ -1,6 +1,8 @@
 import random
 
 class Group:
+    """ Class to represent a group """
+    
     def __init__(self, group_id):
         self.group_id = group_id
         self.members_list = []
@@ -12,6 +14,8 @@ class Group:
         print(f"Group {self.group_id} members: {self.members_list}")
 
 class Person:
+    """ Class to represent a person """
+    
     def __init__(self, id):
         self.id = id
 
@@ -19,21 +23,30 @@ class Person:
         return f"Member №{self.id}"
 
 class Selection:
+    """ Class to represent results of seeding people into groups """
+    
     def __init__(self, number_of_people, number_of_groups):
         self._number_of_people = number_of_people
         self._number_of_groups = number_of_groups
 
     def create_objects(self, number, cls):
+        
+        """ Create objects (either Person or Group) """
+        
         return [cls(n) for n in range(1, number + 1)]
     
     def setup(self):
+        """ Set up participants and groups """
+        
         participants = self.create_objects(self._number_of_people, Person)
         groups = self.create_objects(self._number_of_groups, Group)
         return participants, groups
 
     def seed_participants(self):
+        """ Randomly distribute participants across groups """
+        
         participants, groups = self.setup()
-        group_size = len(participants) // len(groups)  
+        group_size = len(participants) // len(groups)  # Ensure balanced distribution
         
         for group in groups:
             while len(group.members_list) < group_size and participants:
@@ -42,41 +55,46 @@ class Selection:
 
         return groups
 
-def is_in_the_same_group(member_id1, member_id2, groups):
-    for group in groups:
-        member_ids = [member.id for member in group.members_list]
-        if member_id1 in member_ids and member_id2 in member_ids:
-            return True
-    return False
+class Simulation:
+    """ Class to handle the simulation logic """
+    
+    def __init__(self, trials, number_of_participants, number_of_groups):
+        self.trials = trials
+        self.number_of_participants = number_of_participants
+        self.number_of_groups = number_of_groups
 
-def run_simulation(trials, number_of_participants, number_of_groups):
-    success_count = 0
+    def is_in_the_same_group(self, member_id1, member_id2, groups):
+        """ Check if two members are in the same group """
+        
+        for group in groups:
+            member_ids = [member.id for member in group.members_list]
+            if member_id1 in member_ids and member_id2 in member_ids:
+                return True
+        return False
 
-    for _ in range(trials):
-        selection = Selection(number_of_participants, number_of_groups)
-        groups = selection.seed_participants()
+    def run_simulation(self):
+        """ Run the simulation for a set number of trials """
+        success_count = 0
 
-        # Use the IDs 19 and 20 directly
-        if is_in_the_same_group(19, 20, groups):
-            success_count += 1
+        for _ in range(self.trials):
+            selection = Selection(self.number_of_participants, self.number_of_groups)
+            groups = selection.seed_participants()
 
-    probability = success_count / trials
-    return probability
+            # Check if members 19 and 20 are in the same group
+            if self.is_in_the_same_group(19, 20, groups):
+                success_count += 1
+
+        probability = success_count / self.trials
+        return probability
+
 
 if __name__ == "__main__":
     number_of_participants = 20
     number_of_groups = 2
     trials = 10000  # Number of simulation trials
-    probability = run_simulation(trials,number_of_participants,number_of_groups)
+
+    # Run the simulation
+    simulation = Simulation(trials, number_of_participants, number_of_groups)
+    probability = simulation.run_simulation()
+
     print(f"Probability that 19 and 20 are in the same group: {probability}")
-
-
-    
-            
-                
-        
-
-
-
-
-
